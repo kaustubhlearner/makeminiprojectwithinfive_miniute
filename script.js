3a65496c79a268392b902910798b6e3229c278fc
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-09-11 18:19:01 +0530

    only-click

---
+++ script.js
@@ -0,0 +1,49 @@
+let p =document.querySelector('.first')
+
+ function changecolour(){
+    p.style.backgroundColor=
+    'rgb(' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) + ')'
+}
+changecolour()
+
+
+let b =document.querySelector('.second')
+
+ function changep(){
+    b.style.backgroundColor=
+    'rgb(' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) + ')'
+}
+changep()
+
+
+let a =document.querySelector('.third')
+
+ function changecr(){
+    a.style.backgroundColor=
+    'rgb(' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) + ')'
+}
+changecr()
+
+let c  =document.querySelector('.fourth')
+
+ function changer(){
+    c.style.backgroundColor=
+    'rgb(' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) +
+    ',' + Math.round (Math.random() *255) + ')'
+}
+changer()
+
+
+
+
+
+
+
+
